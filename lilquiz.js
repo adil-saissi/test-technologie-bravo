82f8f07abaff8401b33b8126552d2fb71258d076
adil-saissi <125603753+adil-saissi@users.noreply.github.com>
2023-04-10 02:27:32 +0000

    Add files via upload

---
+++ lilquiz.js
@@ -0,0 +1,31 @@
+const correctAnswers =['C','','A','C','C','B','B','A','B','B','A','','B','B','','C','B','','','C'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+form.addEventListener('submit' , e => {
+    e.preventDefault();
+    
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value,form.q11.value, form.q12.value, form.q13.value, form.q14.value, form.q15.value, form.q16.value, form.q17.value, form.q18.value, form.q19.value, form.q20.value];
+    userAnswers.forEach((answer , index) => {
+        if(answer === correctAnswers[index]){
+            score += 1;
+        }
+    });
+    scrollTo(0,0);
+    
+    result.classList.remove('d-none');
+    
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}`;
+        if(output === score){
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    }, 10);
+});
+
+
+
+
